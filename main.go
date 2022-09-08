c9a8ab1a6892a1db2f4d151e160d792f3bff875a
santooooooo <santo.shunsuke@gmail.com>
2022-09-08 11:56:26 +0900

    change comment

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	env := godotenv.Load()
 	if env != nil {
-		// heroku上だとenvが取得できないことによるエラーでサーバーが停止してしまうため、heroku上のみコメントアウト
+		// heroku上だとenvが取得できないことによるエラーでサーバーが停止してしまうため、heroku上のみコメントアウトしたよ
 		//log.Fatal("Error loading .env file")
 	}
 
